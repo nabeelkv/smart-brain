adf78d72327fdc5cf20137ff244d45c3f4155b51
Nabeel <43858092+nabeelkv@users.noreply.github.com>
2022-09-07 15:39:02 +0530

    changed the width and height of image

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import Tilt from 'react-tilt'
 const Logo = () => {
   return (
     <div className="ma5 mt0 center">
-        <Tilt className="Tilt br2 shadow-2 m2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <Tilt className="Tilt br2 shadow-2 m2" options={{ max : 55 }} style={{ height: 151, width: 151 }} >
           <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px'}} src={logo} alt="logo"/> </div>
         </Tilt>
     </div>
